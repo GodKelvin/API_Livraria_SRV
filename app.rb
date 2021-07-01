2d31b43218ea8091dfe4c79919c424da647aa376
GodKelvin <kelvin.lehrback@gmail.com>
2021-07-01 14:49:43 -0300

    Refatorando codigo

--- app.rb
+++ app.rb
@@ -1,10 +1,5 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'mysql2'
-#set :database, {adapter: "mysql2", database: "livraria_SRV.mysql2"}
-
 #importando as rotas
 require_relative 'lib/routes/init'
-
-#Configuracao do banco de dados
-require_relative 'config/config_db.rb'
--- config/config_db.rb
+++
@@ -1,7 +0,0 @@
-ActiveRecord::Base.establish_connection(
-    :adapter => "mysql2",
-    :host => "localhost",
-    :username => "kelvinbd",
-    :password => "1998",
-    :database => "livraria_SRV"
-);
